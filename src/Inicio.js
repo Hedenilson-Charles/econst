7505b60fb1f0463a07039deab1bd095c14fab8c0
Hedenilson-Charles <105455848+Hedenilson-Charles@users.noreply.github.com>
2022-06-02 14:50:55 -0300

    Inicio
    Feitos os cards do inicio

--- src/Inicio.js
+++ src/Inicio.js
@@ -2,9 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Inicio = () => {
+
+    require('./estilos/inicio.css');
     return ( 
-        <div>
-            <p>Inicio</p>
+        <div className="Inicio">
+            <div className="CardProduto">
+
+                <p>Texto Exemplo</p>
+                <p>Preço Qualquer</p>
+                <img className="Imagem" src="https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg"/>
+
+            </div>
+            <div className="CardProduto">
+
+                <p>Texto Exemplo</p>
+                <p>Preço Qualquer</p>
+                <img className="Imagem" src="https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg"/>
+
+            </div>
+            <div className="CardProduto">
+
+                <p>Texto Exemplo</p>
+                <p>Preço Qualquer</p>
+                <img className="Imagem" src="https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg"/>
+
+            </div>
         </div>
      );
 }
