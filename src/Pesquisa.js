4296cd8a54eed1973e6a88a25775eea3875546a3
Hedenilson-Charles <105455848+Hedenilson-Charles@users.noreply.github.com>
2022-06-03 14:42:50 -0300

    s
    s

--- src/Pesquisa.js
+++ src/Pesquisa.js
@@ -2,9 +2,79 @@ import React from "react";
 
 const Pesquisa = () => {
 
+    require('./estilos/pesquisa.css');
+
     return(
-        <div>
-            <p>Tela de Pesquisa</p>
+
+        <div className="Pesquisa">
+
+
+        <div className="Resultado">
+            <div className="blocoImagem">
+                <img src="https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg"/>
+            </div>
+
+            <div className="blocoTextoBotao">
+                <p>Texto descricional qualquer usado apenas para ocupar espaço e dessa forma, observar o comportamento do card quando usado com um texto</p>
+                <button>Le botão</button>
+            </div>
+        </div>
+
+        <div className="Resultado">
+            <div className="blocoImagem">
+                <img src="https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg"/>
+            </div>
+
+            <div className="blocoTextoBotao">
+                <p>Texto descricional qualquer usado apenas para ocupar espaço e dessa forma, observar o comportamento do card quando usado com um texto</p>
+                <button>Le botão</button>
+            </div>
+        </div>
+
+        <div className="Resultado">
+            <div className="blocoImagem">
+                <img src="https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg"/>
+            </div>
+
+            <div className="blocoTextoBotao">
+                <p>Texto descricional qualquer usado apenas para ocupar espaço e dessa forma, observar o comportamento do card quando usado com um texto</p>
+                <button>Le botão</button>
+            </div>
+        </div>
+
+        <div className="Resultado">
+            <div className="blocoImagem">
+                <img src="https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg"/>
+            </div>
+
+            <div className="blocoTextoBotao">
+                <p>Texto descricional qualquer usado apenas para ocupar espaço e dessa forma, observar o comportamento do card quando usado com um texto</p>
+                <button>Le botão</button>
+            </div>
+        </div>
+
+        <div className="Resultado">
+            <div className="blocoImagem">
+                <img src="https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg"/>
+            </div>
+
+            <div className="blocoTextoBotao">
+                <p>Texto descricional qualquer usado apenas para ocupar espaço e dessa forma, observar o comportamento do card quando usado com um texto</p>
+                <button>Le botão</button>
+            </div>
+        </div>
+
+        <div className="Resultado">
+            <div className="blocoImagem">
+                <img src="https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg"/>
+            </div>
+
+            <div className="blocoTextoBotao">
+                <p>Texto descricional qualquer usado apenas para ocupar espaço e dessa forma, observar o comportamento do card quando usado com um texto</p>
+                <button>Le botão</button>
+            </div>
+        </div>
+
         </div>
     );
 }
--- src/Rodape.js
+++ src/Rodape.js
@@ -11,3 +11,4 @@ const Rodape = () => {
 }
  
 export default Rodape;
+
