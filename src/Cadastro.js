1ab56398a3c392f4244f4cba9ce8e716157100ce
Sophie016 <105455939+Sophie016@users.noreply.github.com>
2022-06-07 16:17:25 -0300

    hjd
    hfhjjjj

--- src/Cadastro.js
+++ src/Cadastro.js
@@ -4,17 +4,18 @@ import { Link } from "react-router-dom";
 const Cadastro = () => {
     return ( 
 
-         <div className="Login">
+         <div className="Cadastro">
             <div>
-                <h2> Login</h2>
+                <h2>Cadastro</h2>
+                <h3>Insira seus dados abaixo:</h3>
                 <br/>
                 <input placeholder = "Nome..."/>
                 <br/>
-                <input placeholder = "Insira sua senha..."/>
+                <input placeholder = "CPF"/>
                 <br/>
             </div>
         </div>
      );
 }
  
-export default Cadastro;
+export default Cadastro;                                
