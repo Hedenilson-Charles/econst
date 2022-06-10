924d26962936f0423e0bd93699183380ac2b5f95
Sophie016 <105455939+Sophie016@users.noreply.github.com>
2022-06-10 16:52:49 -0300

    5t35y3h
    t3434ty3

--- src/Painel.js
+++ src/Painel.js
@@ -5,12 +5,21 @@ const Painel = () => {
     require('./estilos/Painel.css');
     
     return(
-        <div className='Painel'>
-        <div>
+        <div className="Painel">
+        
+            <div>
+            <img src='https://i.imgur.com/HAA9XbR.png'/>
+            <input placeholder = "Endereço:" />
+            <input placeholder = "E-mail:" />
+            <input placeholder = "CPF:" />
+
+                <div>
+                <img src='https://th.bing.com/th/id/OIP.6TwhoOQY72DwcJBG_kcwZwHaHa?pid=ImgDetrs=1.jpg'/>
+                <h3>Nome do Usuário</h3>
+                <button>🖊️</button>
+                
+                </div>
             
-            <p>Endereço:</p>
-            <p>E-mail:</p>
-            <p>CPF:</p>
         </div>
         </div>
     );
