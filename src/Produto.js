26e31512f7c8c4f56a2d843247446ac41a1badf1
Hedenilson-Charles <105455848+Hedenilson-Charles@users.noreply.github.com>
2022-06-13 14:25:07 -0300

    d
    d

--- src/Produto.js
+++ src/Produto.js
@@ -6,14 +6,6 @@ const Produto = () => {
 
     return(
         <div className="Produto">
-            <div className="Container">
-                <div className="BlocoEsquerdo">
-                    <img className="ImagemPrincipal" src="https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg"/>
-                </div>
-                <div className="BlocoDireito">
-                    <p>Texto exemplo</p>
-                </div>
-            </div>
         </div>
     );
 }
