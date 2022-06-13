50e65aacb507d65017d6e1cc0ddbb20d84db70c8
Hedenilson-Charles <105455848+Hedenilson-Charles@users.noreply.github.com>
2022-06-13 13:24:48 -0300

    w
    w

--- src/Usuario.js
+++ src/Usuario.js
@@ -3,9 +3,21 @@ import { Link } from "react-router-dom";
 
 const Usuario = () => {
 
+    require('./estilos/usuario.css');
+
     return(
-        <div>
-            <p>tela de usuario</p>
+        <div className="Usuario">
+            <div className="BlocoEsquerdo">
+                <img src="https://i.imgur.com/HAA9XbR.png"/>
+                <p><b>Endereço:</b> Rua Fulano de X número X</p>
+                <p><b>E-mail:</b> fulanodasilva@gmail.com</p>
+            </div>
+            <div className="BlocoDireito">
+                <img src="https://cdn.onlinewebfonts.com/svg/img_184513.png"/>
+                <p>Fulano da Silva</p>
+            
+            
+            </div>
          
         </div>
     );
