f40392dcc64902639d094c71c013858921ec6b4e
Hedenilson-Charles <105455848+Hedenilson-Charles@users.noreply.github.com>
2022-06-13 14:15:09 -0300

    d
    d

--- src/Produto.js
+++ src/Produto.js
@@ -2,9 +2,18 @@ import React from "react";
 
 const Produto = () => {
 
+    require('./estilos/produto.css');
+
     return(
-        <div>
-            <p>Esta é a tela de produto</p>
+        <div className="Produto">
+            <div className="Container">
+                <div className="BlocoEsquerdo">
+                    <img className="ImagemPrincipal" src="https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg"/>
+                </div>
+                <div className="BlocoDireito">
+                    <p>Texto exemplo</p>
+                </div>
+            </div>
         </div>
     );
 }
