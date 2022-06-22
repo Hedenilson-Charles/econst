4f4cd7df189f23f3790fe2aa296a3cacb66da84a
Sophie016 <105455939+Sophie016@users.noreply.github.com>
2022-06-22 16:21:08 -0300

    02
    592

--- src/Carrinho.js
+++ src/Carrinho.js
@@ -7,43 +7,61 @@ const Carrinho = () => {
 
     return(
         <div className='Carrinho'>
-
         <div className='Container'>
-        <div className='BlocoImagem'>
-        <p> Loja de Fulano</p>
-        <hr/>
-        <img src='https://http2.mlstatic.com/bolsa-gucci-marmont-matelass-preta-original-na-caixa-D_NQ_NP_732400-MLB26747258744_022018-F.jpg'/>
-
-
         
-        <Link className="Link" to={'/produto'}>
-                <div className="Resultado">
+        <h3> eConst.com</h3>
+            <Link className="Link" to={'/produto'}>
+                    <div className="Resultado">
                     <div className="blocoImagem">
-                        <img src="https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg"/>
-                    </div>
+                    <img src="https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg"/></div>
 
-                    <div className="blocoTextoBotao">
-                        <p>Texto descricional qualquer usado apenas para ocupar espaço e dessa forma, observar o comportamento do card quando usado com um texto</p>
-                        
-                    </div>
+                <div className='Blocobutton'>    
+                    <button> Cancelar</button> 
                 </div>
-                </Link>
-        
-        </div>
-      
+                     
+                   
+                    </div>
 
-        <div className='BlocoEsquerdo'></div>
-        <button> Clique aqui</button>
-        <button> Clique aqui</button>
-        <div className='BlocoDireito'>
-               
-
+            </Link>
                 
+            <Link className="Link" to={'/produto'}>
+                <div className="Resultado">
+                <div className="blocoImagem">
+                <img src="https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg"/>  </div>
+                           
+                <div className='Blocobutton'>    
+                 <button> Cancelar</button> 
+                </div>
+                
+
+                     
+                    </div>
+            </Link>
+
+          
+            <Link className="Link" to={'/produto'}>
+                    <div className="Resultado">
+                    <div className="blocoImagem">
+                    <img src="https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg"/></div>
+
+                <div className='Blocobutton'>    
+                    <button> Cancelar</button> 
+                </div>
+                     
+                   
+                    </div>
+
+            </Link>
+
+                </div>
+
+
+    
         </div>
-        </div>
+        
 
             
-        </div>
+        
     );
 }
 
