7ca361aec48621f5678428f926f06c73af8250fb
Hedenilson-Charles <105455848+Hedenilson-Charles@users.noreply.github.com>
2022-06-27 16:54:29 -0300

    d
    d

--- src/Produto.js
+++ src/Produto.js
@@ -4,7 +4,6 @@ import { useParams } from "react-router-dom";
 const Produto = () => {
 
     const { id } = useParams();
-    console.log(id);
 
     require('./estilos/produto.css');
 
@@ -22,13 +21,14 @@ const Produto = () => {
 
     //COMEÇANDO CONEXÃO COM O BANCO
 
-    const [produto , imprimeProduto] = useState([]);
+    const [produto , pegaProduto] = useState([]);
 
 
     const axios = require('axios').default;
 
     React.useEffect ( () => {
-console.log(id)
+
+
         axios.get('http://localhost:3001/produto/'+ id)
 
         .then(function (response) {
@@ -36,8 +36,8 @@ console.log(id)
             const dados = response.data;
             
             
-            imprimeProduto(dados);
-            console.log(produto);
+            pegaProduto(dados);
+            
             
     
         })
@@ -49,56 +49,56 @@ console.log(id)
     },[])
 
 
+
     return(
         <div className="Produto">
 
-            <p>{}</p>
-            
+            {produto == 0? "Carregando...":  
+
+            <div className="Produto">
+                <div className="Bloco1">
+
+                    <div className="BlocoImagem1">
+
+                        <img onClick={(e)=>MudarImagem(e)} alt="Imagem1"  src="https://static.riachuelo.com.br/RCHLO/13174711003/portrait/f29e0008f49f3aede5a8e3a3686faae82f1f024a.jpg?imwidth=400"/>
+                        <img onClick={(e)=>MudarImagem(e)}  alt="Imagem2" src="https://static.riachuelo.com.br/RCHLO/13174711003/portrait/254a72bf1aa30d4dc96bf8292b9695bb73470216.jpg?imwidth=70"/>
+                        <img onClick={(e)=>MudarImagem(e)} alt="Imagem3" src="https://static.riachuelo.com.br/RCHLO/13174711003/portrait/d76f0fe59d365fd18a77dbd992ed59b4fdc713be.jpg?imwidth=70"/>
 
 
-            <div className="Bloco1">
-
-                <div className="BlocoImagem1">
-
-                    <img onClick={(e)=>MudarImagem(e)} alt="Imagem1"  src="https://static.riachuelo.com.br/RCHLO/13174711003/portrait/f29e0008f49f3aede5a8e3a3686faae82f1f024a.jpg?imwidth=400"/>
-                    <img onClick={(e)=>MudarImagem(e)}  alt="Imagem2" src="https://static.riachuelo.com.br/RCHLO/13174711003/portrait/254a72bf1aa30d4dc96bf8292b9695bb73470216.jpg?imwidth=70"/>
-                    <img onClick={(e)=>MudarImagem(e)} alt="Imagem3" src="https://static.riachuelo.com.br/RCHLO/13174711003/portrait/d76f0fe59d365fd18a77dbd992ed59b4fdc713be.jpg?imwidth=70"/>
-
-
-                </div>
-                
-                <div className="BlocoImagem">
-
-                <img id="imgPrincipal" alt="Imagem" src="https://static.riachuelo.com.br/RCHLO/13174711003/portrait/f29e0008f49f3aede5a8e3a3686faae82f1f024a.jpg?imwidth=70"/>
-
-                </div>
-
-            <div className="Geral">
-                    <h3> Nome do Produto</h3>
-                    <br/>
-                    <p>R$00,00</p>
-                    <br/>
-                    <p> <b>Tamanho:</b> XG</p>
-                    <br/>
-                    <p> <b>Quantidade:</b> 100 </p>
-
+                    </div>
                     
+                    <div className="BlocoImagem">
 
-                <div className="BlocoBotao">
-                    <button className="Botão2">Comprar agora</button>
-                    <br/>
-                    <button className="Botão">Adicionar ao Carrinho</button>
+                    <img id="imgPrincipal" alt="Imagem" src="https://static.riachuelo.com.br/RCHLO/13174711003/portrait/f29e0008f49f3aede5a8e3a3686faae82f1f024a.jpg?imwidth=70"/>
+
+                    </div>
+
+                <div className="Geral">
+                        <h3> {produto[0].nome}</h3>
+                        <br/>
+                        <p>R${produto[0].preco}</p>
+                        <br/>
+                        <p> <b>Quantidade:</b> 100 </p>
+
+                        
+
+                    <div className="BlocoBotao">
+                        <button className="Botão2">Comprar agora</button>
+                        <br/>
+                        <button className="Botão">Adicionar ao Carrinho</button>
+                    </div>
+
+                </div>
                 </div>
 
-            </div>
-            </div>
+                <div className="Descricao">
+                    <p>{produto[0].descricao}</p>
 
-            <div className="Descricao">
-                <p>Descrição do produto</p>
-
+                </div>
             </div>
+            }
             
-
+    
 
            
         </div>
