d3b007ef6ca6faa142a19bb3b205b7a2284b369f
Yasmin-Hermano <105455913+Yasmin-Hermano@users.noreply.github.com>
2022-06-27 16:55:29 -0300

    xadcf
    cfweffff

--- src/Carrinho.js
+++ src/Carrinho.js
@@ -5,11 +5,32 @@ const Carrinho = () => {
 
     require('./estilos/carrinho.css');
 
+    const [quantidade , alteraQuantidade] = React.useState(0);
+
 
     return(
         <div className='Carrinho'>
 
         <Link className="Link" to={'/produto'}> </Link>
+
+        <div className="Elementos">
+            <img alt='Compras' src='https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg'/>
+
+            <p>Descrição para o rpoduto de teste XG blá blá blá blá e é isso, não sei o que estou fazendo. Obrigada!</p>
+
+            <div className="selecionador">
+
+            <p> Quantidade de produto:
+                <br/> 
+                <button onClick={() => alteraQuantidade( quantidade - 1)}><b>-</b></button> {quantidade} <button onClick={()=> alteraQuantidade( quantidade + 1)}><b>+</b></button></p>
+
+                <p>Preço</p>
+
+            </div>
+
+        </div>
+
+
         
        
 
@@ -23,6 +44,16 @@ const Carrinho = () => {
         
 
             
+
+
+
+
+
+
+
+
+
+
        </div> 
     );
 }
