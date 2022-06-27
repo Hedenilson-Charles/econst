0a4c8b1f644963275b7ae3e72e880f351bccd7e9
Sophie016 <105455939+Sophie016@users.noreply.github.com>
2022-06-27 16:43:23 -0300

    kikjwe
    pop

--- src/Carrinho.js
+++ src/Carrinho.js
@@ -5,82 +5,25 @@ const Carrinho = () => {
 
     require('./estilos/carrinho.css');
 
+
     return(
         <div className='Carrinho'>
-        <div className='Container'>
-            
 
-              <p1> produto </p1>
+        <Link className="Link" to={'/produto'}> </Link>
+        
+       
 
-                <p> preço</p>
-
-            
-            <Link className="Link" to={'/produto'}>
-                    <div className="Resultado">
-                    <div className="blocoImagem">
-                    <img src="https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg"/></div>
+           
                 
-                        
-
-                <div>    
-                <Link className="Link" to={'/produto'}> </Link>
-                <p> Bala de Gelatina e Marshmallows Dentadura 90g - Fini</p>
-                </div>
-                     
-                <div className='Blocobutton'>    
-                    <button> Remover</button> 
-                </div>
                    
-                    </div>
-                <hr/>
-            </Link>
-                
-            <Link className="Link" to={'/produto'}>
-                <div className="Resultado">
-                <div className="blocoImagem">
-                <img src="https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg"/>  </div>
-                           
-                
+        
+            
 
-                <div>    
-                <Link className="Link" to={'/produto'}> </Link>
-                <p> Bala de Gelatina e Marshmallows Dentadura 90g - Fini</p>
-                </div>
-
-                <div className='Blocobutton'>    
-                 <button> Remover</button> 
-                </div>
-                     
-                    </div>
-            </Link>
-
-                <hr/>
-            <Link className="Link" to={'/produto'}>
-                    <div className="Resultado">
-                    <div className="blocoImagem">
-                    <img src="https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg"/></div>
-
-                     
-                <div>    
-                <Link className="Link" to={'/produto'}> </Link>
-                <p> Bala de Gelatina e Marshmallows Dentadura 90g - Fini</p>
-                </div>
-                     
-                <div className='Blocobutton'>    
-                    <button> Remover</button> 
-                </div>
-
-                    </div>
-            <hr/>
-            </Link>
-
-            </div>
-
-        </div>
+               
         
 
             
-        
+       </div> 
     );
 }
 
