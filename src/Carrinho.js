18c90e5af74e6f2289d6091189bd859342e0ea11
Yasmin-Hermano <105455913+Yasmin-Hermano@users.noreply.github.com>
2022-06-28 18:11:39 -0300

    115
    2446.9

--- src/Carrinho.js
+++ src/Carrinho.js
@@ -12,9 +12,16 @@ const Carrinho = () => {
         <div className='Carrinho'>
 
 
-            <div>
+            <div className="Guia">
             <p>Produto</p>
+
+            <div className='Guia2'>
             <p>Preço</p>
+            <p>Qtd.</p>
+            <hr/>
+            </div>
+
+            
             </div>
 
         <Link className="Link" to={'/produto'}> </Link>
@@ -22,18 +29,30 @@ const Carrinho = () => {
         <div className="Elementos">
             <img alt='Compras' src='https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg'/>
 
+            <Link className="Link" to={'/produto'}> 
+
+            <div className='Descrição'>
+                
             <p>Descrição para o rpoduto de teste XG blá blá blá blá e é isso, não sei o que estou fazendo. Obrigada!</p>
+            
+            </div>
+            </Link>
 
-            <div className="selecionador">
+            <div className='preco'>
+                <p>R$00,00</p>
+            </div>
 
-            <p> Quantidade de produto:
+            <div className="quantidade">
+
+            <p> Quantidade de produto
                 <br/> 
                 <button onClick={() => alteraQuantidade( quantidade - 1)}><b>-</b></button> {quantidade} <button onClick={()=> alteraQuantidade( quantidade + 1)}><b>+</b></button></p>
-
-               
-             
-
             </div>
+
+            <div className='remover'>
+               <button>Remover</button>
+            </div>
+
 
         </div>
 
