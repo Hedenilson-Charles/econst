1b5061f1db2837bac3ddbd950be457e20a18f17f
guilherme-ocampos <105455922+guilherme-ocampos@users.noreply.github.com>
2022-06-29 14:42:09 -0300

    gui
    efth

--- src/Pedidos.js
+++ src/Pedidos.js
@@ -1,55 +1,32 @@
 import React from "react";
 
-
 const Pedidos = () => {
-    
-    const [produtos, imprimeProdutos] = React.useState([]);
-    const axios = require('axios').default;
 
-    React.useEffect ( () => {
-
-        axios.get('http://localhost:3001/')
-
-        .then(function (response) {
-
-            const dados = response.data;
-            
-            
-            imprimeProdutos(dados);
-        
-      
-        })
-
-        .catch(function (error) {
-            console.log(error);
-        })
-
-    }, [])
-
-
+  
     require('./estilos/pedidos.css');
     
     return ( 
         
-     <div classname="pedidos">
+        
+    <div classname="pedidos">
 
          <p>Tela de pedidos</p>
 
-    <form>
-       <h2>Compars recentes</h2>
-           
-        <div id="container-a"></div><div id="container-b"></div>
-             
-        <div id="container-a"></div><div id="container-b"></div>
-           
-        <div id="container-a"></div><div id="container-b"></div>
-            
-        <div id="container-a"></div><div id="container-b"></div>
-            
-        <div id="container-a"></div><div id="container-b"></div>
-    </form>  
-</div>
+        <form>
+             <h2>Compars recentes</h2>
+                
+                <div id="container-a"></div><div id="container-b"></div>
+                    
+                <div id="container-a"></div><div id="container-b"></div>
+                
+                <div id="container-a"></div><div id="container-b"></div>
+                    
+                <div id="container-a"></div><div id="container-b"></div>
+                    
+                <div id="container-a"></div><div id="container-b"></div>
+        </form>  
+    </div>
 
-);}
+    );}
  
 export default Pedidos;
