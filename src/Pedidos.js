fdf6eb2f1321ec04959ed85fbd3d526ab24750dc
guilherme-ocampos <105455922+guilherme-ocampos@users.noreply.github.com>
2022-06-29 15:09:45 -0300

    ele
    gui

--- src/Pedidos.js
+++ src/Pedidos.js
@@ -2,27 +2,16 @@ import React from "react";
 
 const Pedidos = () => {
 
-  
-    require('./estilos/pedidos.css');
-    
+
     return ( 
         
         
     <div classname="pedidos">
 
-         <p>Tela de pedidos</p>
+         <p>Tela de pedidos</p> 
 
         <form>
-             <h2>Compars recentes</h2>
-                
-                <div id="container-a"></div><div id="container-b"></div>
-                    
-                <div id="container-a"></div><div id="container-b"></div>
-                
-                <div id="container-a"></div><div id="container-b"></div>
-                    
-                <div id="container-a"></div><div id="container-b"></div>
-                    
+               <h2>Compars recentes</h2>
                 <div id="container-a"></div><div id="container-b"></div>
         </form>  
     </div>
